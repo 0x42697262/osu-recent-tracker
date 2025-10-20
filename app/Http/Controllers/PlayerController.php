b324f3f5a9298fb8648bbd38c3292119670a2fd4
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2025-10-20 11:44:06 +0900

    add api route support
    php artisan install:api

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+use App\Models\Player;
+
+class PlayerController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $players = Player::all();
+        return response()->json($players);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PlayerController;
+use App\Models\Player;
+
+Route::prefix('v1')->group(function (){
+    Route::get('players', [PlayerController::class, 'index']);
+});
